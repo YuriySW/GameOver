6eaeed0af18a69d85683041fed7c12bb880a6def
YuriySW <yuriy.s@my.com>
2024-06-16 03:34:54 +0500

    lesson 6.3 hw 1.6

--- script/modules/acc.js
+++ script/modules/acc.js
@@ -1,5 +1,42 @@
-const items = document.querySelectorAll('.questions__item ');
+// const items = document.querySelectorAll('.questions__item ');
+// const textWrap = document.querySelectorAll('.questions__text-wrapp');
+
+// export const accOpen = () => {
+//   items.forEach((btn, index) => {
+//     btn.addEventListener('click', () => {
+//       textWrap.forEach((el, i) => {
+//         if (i !== index) {
+//           el.classList.remove('questions__item_actv');
+//           el.style.maxHeight = null;
+//         }
+//       });
+
+//       const panel = textWrap[index];
+//       const isActive = panel.classList.toggle('questions__item_actv');
+//       if (isActive) {
+//         panel.style.maxHeight = panel.scrollHeight + 'px';
+//       } else {
+//         panel.style.maxHeight = null;
+//       }
+//     });
+//   });
+
+//   document.addEventListener('click', (event) => {
+//     if (!event.target.closest('.questions__item')) {
+//       textWrap.forEach((el) => {
+//         el.classList.remove('questions__item_actv');
+//         el.style.maxHeight = null;
+//       });
+//     }
+//   });
+// };
+
+// accOpen();
+
+const items = document.querySelectorAll('.questions__item');
 const textWrap = document.querySelectorAll('.questions__text-wrapp');
+const purpleItems = document.querySelectorAll('.questions__items_svg-purple');
+const whiteItems = document.querySelectorAll('.questions__items_svg-white');
 
 export const accOpen = () => {
   items.forEach((btn, index) => {
@@ -17,6 +54,8 @@ export const accOpen = () => {
         panel.style.maxHeight = panel.scrollHeight + 'px';
       } else {
         panel.style.maxHeight = null;
+        purpleItems.forEach((item) => item.blur());
+        whiteItems.forEach((item) => item.blur());
       }
     });
   });
@@ -32,43 +71,3 @@ export const accOpen = () => {
 };
 
 accOpen();
-
-// const items = document.querySelectorAll('.questions__item');
-// const textWrap = document.querySelectorAll('.questions__text-wrapp');
-
-// export const accOpen = () => {
-//   items.forEach((btn, index) => {
-//     btn.addEventListener('click', () => {
-//       textWrap.forEach((el, i) => {
-//         if (i !== index) {
-//           el.classList.remove('questions__item_actv');
-//           el.style.maxHeight = null;
-//           el.parentNode.querySelector('svg').classList.remove('rotated');
-//         }
-//       });
-
-//       const panel = textWrap[index];
-//       const isActive = panel.classList.toggle('questions__item_actv');
-//       const svgIcon = panel.parentNode.querySelector('svg');
-//       if (isActive) {
-//         panel.style.maxHeight = panel.scrollHeight + 'px';
-//         svgIcon.classList.add('rotated');
-//       } else {
-//         panel.style.maxHeight = null;
-//         svgIcon.classList.remove('rotated');
-//       }
-//     });
-//   });
-
-//   document.addEventListener('click', (event) => {
-//     if (!event.target.closest('.questions__item')) {
-//       textWrap.forEach((el) => {
-//         el.classList.remove('questions__item_actv');
-//         el.style.maxHeight = null;
-//         el.parentNode.querySelector('svg').classList.remove('rotated');
-//       });
-//     }
-//   });
-// };
-
-// accOpen();
